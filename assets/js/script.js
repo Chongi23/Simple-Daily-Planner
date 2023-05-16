6b89f6a5e20316231d22c37c323eb0e1b0720c21
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-05-16 14:24:23 -0500

    latest update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,37 +4,66 @@
 
 //This is the same as $(function(){});--Just longer for begginer developers.
 $(document).ready(function () {
+
+  
+
   // TODO: Add a listener for click events on the save button.(DONE) This code should
   // use the id in the containing time-block as a key to save the user input in
+  for (var i = 9; i < 18; i++) {
+    var currentHour = new Date().getHours();
+    var colorKey = "";
+
+
+    if(currentHour>i){
+      colorKey = "past"
+    }
+    else if (currentHour === i){
+      colorKey = "present"
+    }
+     else {
+      colorKey = "future"
+     }
+console.log(currentHour)
+
+    var rowDiv = $("<div>").addClass("row time-block "+colorKey).attr("id", "hour " + i)
+    var hourDiv = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(i)
+
+var textarea = $("<textarea>").addClass("col-8 col-md-10 description").attr("rows","3").val(localStorage.getItem("hour-"+i))
+var btn = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label","save")
+   var icon = $("<i>").addClass("fas fa-save").attr("aria-hidden","true")
+
+$(".main").append(rowDiv.append(hourDiv,textarea,btn.append(icon)))
+  }
   // local storage.
-  $(".saveBtn").on("click", function(){
+  $(".saveBtn").on("click", function () {
 
-                //$(this)=.saveBtn.
-                //.siblings oare siblings of "description" class ,that is where user enters text on webpage
-               //to be saved for schedule. 
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        // The id is the key for different times,The parent of $(this),selected by its id
-        // hour-9,hour-10 etc.
+    //$(this)=.saveBtn.
+    //.siblings oare siblings of "description" class ,that is where user enters text on webpage
+    //to be saved for schedule. 
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+   
+    console.log(text, time)
+    localStorage.setItem(time, text)
+    //Save to local storege
 
-        //Save to local storege
-        localStorage=setItem(text,time);
-        //The setItem()method sets the value of the specific Storage Object item.
-        //The setItem() methos belongs to Storage Object, which can be either a localStorage
-        //object or a sessionStorage...local is requested of us.
+
   })
-  
+
+  //updated,with help of tutor
+
   //HINT: What does `this` reference in the click listener
   // function?This is to say: inside of the click event handler,"this" refers to the element that was clicked.
   //When you put "this"inside of:$(), you "wrap" the JavaScript "this" keyword which adds a number of properties and methods.
+  
   // How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
 
-  // 05/12/23 -First set of To-Do Done.
+ 
 
-  
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
